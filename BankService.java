f581260540d8e7a47526b6654e6cd41d193659f9
gaganbidaralli <161804569+gaganbidaralli@users.noreply.github.com>
2026-01-29 09:43:58 +0530

    Add files via upload

---
+++ BankService.java
@@ -0,0 +1,36 @@
+public class BankService 
+{
+    static class BankAccount
+    {
+        private double balance;
+        
+        BankAccount(double b)
+        {
+            if(b<0) throw new IllegalArgumentException();
+                balance=b;
+        }
+        
+        void deposit(double a)
+        {
+            if(a<0) throw new IllegalArgumentException();
+                balance +=a;
+        }
+        
+        void withdrawl(double a)
+        {
+            if(a<0) throw new IllegalArgumentException();
+                balance -=a;
+        }
+        
+        double getBalance()
+        {
+            return balance;
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        BankAccount bc=new BankAccount(5000);
+        System.out.println(bc.getBalance());
+    }
+}
